636a5d24b190618b28e179c62eb16f1cb66b2c41
FrotaLucas <lucasdiasfrota@gmail.com>
2025-11-22 14:13:41 +0100

    Create interface IWeatherRepository

---
+++ BetterCodePaulChallange.Domain/Contracts/Repository/IWeatherRepository.cs
@@ -0,0 +1,9 @@
+﻿using BetterCodePaulChallange.Domain.Entities;
+
+namespace BetterCodePaulChallange.Domain.Contracts.Repository
+{
+    public interface IWeatherRepository
+    {
+        public List<WeatherData> GetWheaterDatas();
+    }
+}
