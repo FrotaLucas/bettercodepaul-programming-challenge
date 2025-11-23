2e7541506ee2ecd18ef176f3cc7f26a3ee794a15
FrotaLucas <lucasdiasfrota@gmail.com>
2025-11-23 12:22:47 +0100

    remove unused folders

--- BetterCodePaulChallange.Domain/Contracts/Repository/IWeatherRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using BetterCodePaulChallange.Domain.Entities;
-
-namespace BetterCodePaulChallange.Domain.Contracts.Repository
-{
-    public interface IWeatherRepository
-    {
-        public List<WeatherData> GetWheaterDatas();
-    }
-}
