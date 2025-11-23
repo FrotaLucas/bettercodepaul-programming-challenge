8c559f9de93803a2c23bcdac35086a7625419fb4
FrotaLucas <lucasdiasfrota@gmail.com>
2025-11-23 14:29:51 +0100

    finish menu

--- BetterCodePaulChallange.ConsoleApp/Program.cs
+++ BetterCodePaulChallange.ConsoleApp/Program.cs
@@ -2,16 +2,53 @@
 using BetterCodePaulChallange.ConsoleApp.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-internal class Program
+public class Program
 {
-    private static void Main(string[] args)
+    public static void Main(string[] args)
     {
         var host = Startup.CreateHost();
         var weatherService = host.Services.GetRequiredService<IWeatherService>();
 
-        var data = weatherService.GetWeatherData();
+        string greeting = DateTime.Now.Hour < 12
+            ? "Good morning"
+            : DateTime.Now.Hour < 18
+                ? "Good afternoon"
+                : "Good evening";
 
+        Console.WriteLine($"\n=== {greeting}, welcome to the Weather App ===\n");
 
-        Console.WriteLine(data);
+        bool showMenu = true;
+
+        while (showMenu)
+        {
+            Console.WriteLine("Choose an option:\n");
+            Console.WriteLine(" 1 - Day with the smallest temperature spread");
+            Console.WriteLine(" 2 - Lowest historical temperature");
+            Console.WriteLine(" 0 - Exit");
+            Console.Write("\nOption: ");
+
+            string? commandLine = Console.ReadLine();
+
+            switch (commandLine)
+            {
+                case "1":
+                    var op1 = weatherService.GetWeatherData();
+                    Console.WriteLine($"\nThe day with the smallest temperature spread is: {op1.Day}\n");
+                    break;
+
+                case "2":
+                    Console.WriteLine("\nLowest temperature feature is not implemented yet.\n");
+                    break;
+
+                case "0":
+                    showMenu = false;
+                    Console.WriteLine("\nExiting... Goodbye!\n");
+                    break;
+
+                default:
+                    Console.WriteLine("\nInvalid option. Try again.\n");
+                    break;
+            }
+        }
     }
 }
